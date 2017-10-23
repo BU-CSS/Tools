ae31007d33f617ba9c91a687479b8158465d2395
Alex Seymour <alex@luciddev.tech>
2017-10-23 13:19:47 +0100

    Update puppet_setup.sh

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -10,4 +10,4 @@ then
 fi
 
 sudo -- sh -c -e "echo '192.168.$room.41	bucss-kali-$room-master.pg12.local' >> /etc/hosts";
-echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-kali-$room-master.pg$room.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
+echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-kali-$room-master.pg12.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
