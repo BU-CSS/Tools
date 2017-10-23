a042e9d34f165ee3a0d2388a3784b1ee688ca701
Alex Seymour <alex@luciddev.tech>
2017-10-23 09:25:56 +0100

    Added Kali Puppet setup script

---
+++ puppet_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+room=$1
+pc=$2
+
+echo "bucss-kali-$room-$pc" > /etc/hostname
+
+if [ -n "$(grep puppet /etc/hosts)" ]
+then
+	sed -i "/puppet/d" /etc/hosts
+fi
+
+sudo -- sh -c -e "echo '192.168.$room.41	bucss-kali-$room-master.pg$room.local' >> /etc/hosts";
+echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-kali-$room-master.pg$room.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
