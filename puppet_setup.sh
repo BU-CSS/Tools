def20f052bd42e50521a7fa68b9cfa83d84df43c
Alex Seymour <alex@luciddev.tech>
2017-10-23 13:09:10 +0100

    Update puppet_setup.sh

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -2,12 +2,12 @@
 room=$1
 pc=$2
 
-echo "bucss-kali-$room-$pc" > /etc/hostname
+echo "bucss-kali-12-$pc" > /etc/hostname
 
 if [ -n "$(grep puppet /etc/hosts)" ]
 then
 	sed -i "/puppet/d" /etc/hosts
 fi
 
-sudo -- sh -c -e "echo '192.168.$room.41	bucss-kali-$room-master.pg$room.local' >> /etc/hosts";
+sudo -- sh -c -e "echo '192.168.$room.41	bucss-kali-$room-master.pg12.local' >> /etc/hosts";
 echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-kali-$room-master.pg$room.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
