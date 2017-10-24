6bf25bbb8c253158f3f289932155bb98f980874b
Alex Seymour <alex@luciddev.tech>
2017-10-24 17:53:05 +0100

    Added dev VM puppet setup script

---
+++ puppet_setup_dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+room=$1
+pc=$2
+
+sudo apt install puppet
+echo "bucss-dev-$room-$pc" > /etc/hostname
+
+if [ -n "$(grep puppet /etc/hosts)" ]
+then
+	sed -i "/puppet/d" /etc/hosts
+fi
+
+sudo -- sh -c -e "echo '192.168.$room.42	bucss-dev-$room-master.pg12.local' >> /etc/hosts";
+echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-dev-$room-master.pg12.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
