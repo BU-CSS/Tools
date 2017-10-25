7bf52a3635185a94c3a500d3ca66064afa954664
Alex Seymour <alex@luciddev.tech>
2017-10-25 13:13:03 +0100

    Update puppet_setup.sh

--- puppet_setup.sh
+++ puppet_setup.sh
@@ -11,3 +11,4 @@ fi
 
 sudo -- sh -c -e "echo '192.168.$room.41	bucss-kali-$room-master.pg12.local' >> /etc/hosts";
 echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-kali-$room-master.pg12.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
+puppet resource service puppet ensure=running enable=true
