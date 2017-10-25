9e7d9135563518cacff2c3dd56c14f536d2b6339
Alex Seymour <alex@luciddev.tech>
2017-10-25 13:12:11 +0100

    Update puppet_setup_dev.sh

--- puppet_setup_dev.sh
+++ puppet_setup_dev.sh
@@ -10,5 +10,6 @@ then
 	sudo sed -i "/puppet/d" /etc/hosts
 fi
 
-sudo -- sh -c -e "echo '192.168.$room.42	bucss-dev-$room-master.pg12.local' >> /etc/hosts";
+sudo -- sh -c -e "echo '192.168.$room.42	bucss-dev-$room-master.pg12.local' >> /etc/hosts"
 sudo echo -e "[main]\nssldir = /var/lib/puppet/ssl\nserver = bucss-dev-$room-master.pg12.local\n\n[master]\nvardir = /var/lib/puppet\ncadir = /var/lib/puppet/ssl/ca\ndns_alt_names = puppet" > /etc/puppet/puppet.conf
+sudo puppet resource service puppet ensure=running enable=true
